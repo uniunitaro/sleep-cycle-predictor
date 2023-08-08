a653a064767db3179748a3113f94e97e35df93e1
UniuniTaro <81953006+UniuniTaro@users.noreply.github.com>
2023-08-09 02:00:50 +0900

    next/jestをやめて@swc/jestに node環境でRSCのテストをするときに、子コンポーネントがクライアントコンポーネントだとnext/jestでは動かないため

---
+++ client/jest.config.mjs
@@ -0,0 +1,21 @@
+const config = {
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default config
