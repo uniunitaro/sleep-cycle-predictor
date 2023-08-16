dbc4e98c3371b08e0afe4369a11f36f99b64556a
UniuniTaro <81953006+UniuniTaro@users.noreply.github.com>
2023-08-16 18:44:11 +0900

    redirect

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     // swcPlugins: [['next-superjson-plugin', { excluded: [] }]],
     serverActions: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/QxURPkM1WvVwiytseiF2WbP8GZc2',
+        destination: '/8848af32-69d8-416c-a259-37e9c1582b8b',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
