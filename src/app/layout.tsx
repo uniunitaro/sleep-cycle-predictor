cd678381b1ec7bad59000e1a81450b0260362ecf
UniuniTaro <81953006+UniuniTaro@users.noreply.github.com>
2023-08-27 07:03:32 +0900

    description変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ export const metadata: Metadata = {
     template: '%s - Sleep Predictor',
     absolute: 'Sleep Predictor',
   },
-  description: '毎日ずれてゆくあなたの睡眠サイクルを華麗に予測！',
+  description:
+    '非24時間睡眠覚醒症候群の人をサポートする、睡眠サイクル予測アプリケーション。あなたの体内時計に合わせて、日々のスケジュールを調整しましょう。',
   robots: { index: false },
 }
 
