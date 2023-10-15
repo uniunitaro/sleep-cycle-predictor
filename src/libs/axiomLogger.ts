741cc13ca4a122a63392d0c73082d39015dee23f
uniunitaro <81953006+uniunitaro@users.noreply.github.com>
2023-10-15 13:52:31 +0900

    logger修正

--- src/libs/axiomLogger.ts
+++ src/libs/axiomLogger.ts
@@ -3,22 +3,22 @@ import { Logger } from 'next-axiom'
 export const log = {
   debug: async (message: any, args?: { [key: string]: any }) => {
     const logger = new Logger()
-    logger.debug(message, args)
+    logger.debug(message.toString(), args)
     await logger.flush()
   },
   info: async (message: any, args?: { [key: string]: any }) => {
     const logger = new Logger()
-    logger.info(message, args)
+    logger.info(message.toString(), args)
     await logger.flush()
   },
   warn: async (message: any, args?: { [key: string]: any }) => {
     const logger = new Logger()
-    logger.warn(message, args)
+    logger.warn(message.toString(), args)
     await logger.flush()
   },
   error: async (message: any, args?: { [key: string]: any }) => {
     const logger = new Logger()
-    logger.error(message, args)
+    logger.error(message.toString(), args)
     await logger.flush()
   },
 }
