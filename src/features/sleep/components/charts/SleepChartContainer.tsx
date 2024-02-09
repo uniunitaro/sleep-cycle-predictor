8d40a344b6724f54b6a892db7a8b17f734151ca0
uniunitaro <81953006+uniunitaro@users.noreply.github.com>
2024-02-10 05:20:08 +0900

    listビューのときのスクロールバグを修正

--- src/features/sleep/components/charts/SleepChartContainer.tsx
+++ src/features/sleep/components/charts/SleepChartContainer.tsx
@@ -150,7 +150,13 @@ const SleepChartContainer: FC<{
                     isPublic={isPublic}
                   />
                 </Box>
-                <Box ref={listContainerRef} overflowY="auto" px="6" flex="1">
+                <Box
+                  ref={listContainerRef}
+                  pos="relative"
+                  overflowY="auto"
+                  px="6"
+                  flex="1"
+                >
                   <SleepList
                     sleeps={sleeps}
                     predictions={predictions}
